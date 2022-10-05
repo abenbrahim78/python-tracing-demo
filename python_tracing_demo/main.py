bafa03b14881b1187096b44e389a5067774d8469
abenbrahim78 <abdallah.benbrahim@yahoo.fr>
2022-10-05 18:17:29 +0200

    Update main.py

--- python_tracing_demo/main.py
+++ python_tracing_demo/main.py
@@ -15,7 +15,7 @@ app = FastAPI()
 resource = Resource(attributes={"service.name": "fastapi-tracing-demo"})
 tracer = TracerProvider(resource=resource)
 trace.set_tracer_provider(tracer)
-tracer.add_span_processor(BatchSpanProcessor(OTLPSpanExporter(endpoint="tempo-us-central1.grafana.net:443")))
+tracer.add_span_processor(BatchSpanProcessor(OTLPSpanExporter(endpoint="10.128.3.184:4317")))
 
 LoggingInstrumentor().instrument()
 FastAPIInstrumentor.instrument_app(app, tracer_provider=tracer)
